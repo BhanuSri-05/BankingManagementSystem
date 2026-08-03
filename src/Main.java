16fc0c13bd4876d44fb0c4c4a37c57077f310250
Tavva Bhanu Sri <bhanusri112005@gmail.com>
2026-08-03 21:11:45 +0530

    Add Main class with menu display functionality

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import menu.MainMenu;
+
+
+public class Main {
+    public static void main(String[] args) {
+        
+        MainMenu mainMenu = new MainMenu();
+        mainMenu.showMenu();
+
+    }
+}
